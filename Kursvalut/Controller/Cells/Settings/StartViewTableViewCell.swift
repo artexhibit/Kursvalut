026e881f8bab5d9f7e3c4c30e2da7ce28f643c87
artexhibit <vendabal@yandex.ru>
2022-03-02 22:02:36 +0300

    Created StartViewTableViewController and StartViewTableViewCell in settings view to let user choose which view he wants to see when open app

---
+++ Kursvalut/Controller/Cells/Settings/StartViewTableViewCell.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class StartViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var viewNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ Kursvalut/Controller/VC/Settings/StartViewTableViewController.swift
@@ -0,0 +1,58 @@
+
+import UIKit
+
+class StartViewTableViewController: UITableViewController {
+    
+    let optionsArray = ["Валюты", "Конвертер"]
+    let sectionArray = [(header: "", footer: "Выбранный экран будет открываться сразу после запуска приложения")]
+    var pickedStartView: String {
+        return UserDefaults.standard.string(forKey: "startView") ?? ""
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       return sectionArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return optionsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return sectionArray[section].footer
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "startViewCell", for: indexPath) as! StartViewTableViewCell
+        cell.viewNameLabel.text = optionsArray[indexPath.row]
+        cell.accessoryType = cell.viewNameLabel.text == pickedStartView ? .checkmark : .none
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let cell = tableView.cellForRow(at: indexPath) as? StartViewTableViewCell else { return }
+        let pickedSection = indexPath.section
+        let pickedOption = cell.viewNameLabel.text
+        
+        if cell.accessoryType != .checkmark {
+            for row in 0..<tableView.numberOfRows(inSection: pickedSection) {
+                guard let cell = tableView.cellForRow(at: IndexPath(row: row, section: pickedSection)) else { return }
+                cell.accessoryType = .none
+            }
+            cell.accessoryType = .checkmark
+        }
+        UserDefaults.standard.set(pickedOption, forKey: "startView")
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return tableView.estimatedSectionFooterHeight
+    }
+}
