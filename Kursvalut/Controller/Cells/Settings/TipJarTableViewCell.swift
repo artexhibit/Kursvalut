cb62ddb8edf5a6f48dcbeec8177e85f1def1e833
artexhibit <vendabal@yandex.ru>
2022-03-15 23:10:30 +0300

    Change tipCell design by removing coloured tip price view, add StoreKit and purchase methods

--- Kursvalut/Controller/Cells/Settings/TipJarTableViewCell.swift
+++ Kursvalut/Controller/Cells/Settings/TipJarTableViewCell.swift
@@ -3,13 +3,11 @@ import UIKit
 
 class TipJarTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var tipButton: UIButton!
     @IBOutlet weak var tipNameLabel: UILabel!
     @IBOutlet weak var tipPriceLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        tipButton.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Kursvalut/Controller/VC/Settings/TipJarTableViewController.swift
+++ Kursvalut/Controller/VC/Settings/TipJarTableViewController.swift
@@ -1,32 +1,81 @@
 
 import UIKit
+import StoreKit
 
 class TipJarTableViewController: UITableViewController {
     
     @IBOutlet weak var tableViewFooterLabel: UILabel!
     
     private var currencyManager = CurrencyManager()
-    private let dataArray = [
-        (tipName: "Небольшие чаевые", tipPrice: "149,00 ₽"),
-        (tipName: "Средние чаевые", tipPrice: "499,00 ₽"),
-        (tipName: "Щедрые чаевые", tipPrice: "999,00 ₽")
-    ]
+    private var tipsArray = [SKProduct]()
+    private let tipsID = Set(["ru.igorcodes.kursvalut.smalltip", "ru.igorcodes.kursvalut.mediumtip", "ru.igorcodes.kursvalut.bigtip"])
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        SKPaymentQueue.default().add(self)
         tableViewFooterLabel.text = "Чаевые поддерживают текущую разработку приложения. Спасибо!"
+        fetchTips()
     }
     
     //MARK: - TableView DataSource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return tipsArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tip = tipsArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "tipJarCell", for: indexPath) as! TipJarTableViewCell
-        cell.tipNameLabel.text = dataArray[indexPath.row].tipName
-        cell.tipButton.setTitle(dataArray[indexPath.row].tipPrice, for: .normal)
+        cell.tipNameLabel.text = tip.localizedTitle
+        cell.tipPriceLabel.text = "\(tip.price) \(tip.priceLocale.currencySymbol ?? "$")"
         return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let tipPayment = SKPayment(product: tipsArray[indexPath.row])
+        SKPaymentQueue.default().add(tipPayment)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension TipJarTableViewController: SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    func fetchTips() {
+        if SKPaymentQueue.canMakePayments() {
+            let request = SKProductsRequest(productIdentifiers: tipsID)
+            request.delegate = self
+            request.start()
+        } else {
+            print("You can't make payments")
+        }
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        DispatchQueue.main.async {
+            self.tipsArray = response.products
+            self.tipsArray.sort(by: {$0.price.floatValue < $1.price.floatValue})
+            self.tableView.reloadData()
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .failed:
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .restored:
+                break
+            case .purchasing:
+                break
+            case .deferred:
+                break
+            @unknown default:
+                break
+            }
+        }
     }
 }
