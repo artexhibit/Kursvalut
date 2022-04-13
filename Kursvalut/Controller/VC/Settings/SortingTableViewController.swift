e8f7b1e2bce30a9c3ff5d850da24ba7ac1dbfdeb
artexhibit <vendabal@yandex.ru>
2022-04-13 22:51:24 +0300

    Refactor SortingTableVC code to allow parallel chevron animation and section reloading

--- Kursvalut/Controller/VC/Settings/SortingTableViewController.swift
+++ Kursvalut/Controller/VC/Settings/SortingTableViewController.swift
@@ -29,32 +29,41 @@ class SortingTableViewController: UITableViewController {
     //MARK: - TableView DataSource Methods
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        sections.count
+        return sections.count * 2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let section = sections[section]
-        return section.isOpened ? section.options.count + 1 : 1
+        let virtualSection: Int = section / 2
+        
+        if section % 2 == 0 {
+            return 1
+        } else if sections[virtualSection].isOpened {
+            return sections[virtualSection].options.count
+        } else {
+            return 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
+        let virtualSection: Int = indexPath.section / 2
+        
+        if indexPath.section % 2 == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "mainSortCell", for: indexPath) as! MainSortTableViewCell
-            cell.titleLabel.text = sections[indexPath.section].title
-            cell.subtitleLabel.text = sections[indexPath.section].subtitle
-            cell.proLabel.isHidden = indexPath.section == 3 && !proPurchased ? false : true
+            cell.titleLabel.text = sections[virtualSection].title
+            cell.subtitleLabel.text = sections[virtualSection].subtitle
+            cell.proLabel.isHidden = virtualSection == 3 && !proPurchased ? false : true
             
-            if sections[indexPath.section].isOpened {
-                cell.chevronImage.transform = CGAffineTransform(rotationAngle: .pi/2)
+            if sections[virtualSection].isOpened {
+                cell.chevronImage.transform = CGAffineTransform(rotationAngle: .pi / 2)
             } else {
                 cell.chevronImage.transform = .identity
             }
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "subSortCell", for: indexPath) as! SubSortTableViewCell
-            cell.titleLabel.text = sections[indexPath.section].options[indexPath.row - 1]
+            cell.titleLabel.text = sections[virtualSection].options[indexPath.row]
             
-            if pickedSectionNumber == indexPath.section {
+            if pickedSectionNumber == virtualSection {
                 cell.accessoryType = cell.titleLabel.text == pickedOrder ? .checkmark : .none
             } else {
                 cell.accessoryType = .none
@@ -64,33 +73,39 @@ class SortingTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let title = section == 0 ? "На экране Валюты" : ""
-        return title
+        return section == 0 ? "На экране Валюты" : ""
     }
     
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            sections[indexPath.section].isOpened.toggle()
-            
+        let virtualSection: Int = indexPath.section / 2
+        
+        if indexPath.section % 2 == 0 {
+            sections[virtualSection].isOpened.toggle()
             guard let cell = tableView.cellForRow(at: indexPath) as? MainSortTableViewCell else { return }
-            UIView.animate(withDuration: 0.2) {
-                if self.sections[indexPath.section].isOpened {
-                    cell.chevronImage.transform = CGAffineTransform(rotationAngle: .pi/2)
+            
+            if virtualSection == 3 && !proPurchased {
+                sections[virtualSection].isOpened = false
+                tableView.reloadSections([virtualSection], with: .fade)
+                PopupView().showPopup(title: "Закрыто", message: "Доступно только в Pro", type: .lock)
+            }
+            
+            UIView.animate(withDuration: 0.3) {
+                if self.sections[virtualSection].isOpened {
+                    cell.chevronImage.transform = CGAffineTransform(rotationAngle: .pi / 2)
                 } else {
                     cell.chevronImage.transform = .identity
                 }
-            } completion: { _ in
-                tableView.reloadSections([indexPath.section], with: .none)
             }
+            tableView.reloadSections([indexPath.section + 1], with: .fade)
         } else {
             guard let cell = tableView.cellForRow(at: indexPath) as? SubSortTableViewCell else { return }
-            let pickedSection =  sections[indexPath.section].title
+            let pickedSection =  sections[virtualSection].title
             let pickedOrder = cell.titleLabel.text ?? ""
             
             if cell.accessoryType != .checkmark {
-                for section in 0..<tableView.numberOfSections {
+                for section in 0..<tableView.numberOfSections where section % 2 != 0 {
                     for row in 0..<tableView.numberOfRows(inSection: section) {
                         guard let cell = tableView.cellForRow(at: IndexPath(row: row, section: section)) else { return }
                         cell.accessoryType = .none
@@ -100,13 +115,7 @@ class SortingTableViewController: UITableViewController {
             }
             UserDefaults.standard.set(pickedOrder, forKey: "pickedOrder")
             UserDefaults.standard.set(pickedSection, forKey: "pickedSection")
-            UserDefaults.standard.set(indexPath.section, forKey: "pickedSectionNumber")
-        }
-        
-        if indexPath.section == 3 && !proPurchased {
-            sections[3].isOpened = false
-            tableView.reloadSections([3], with: .none)
-            PopupView().showPopup(title: "Закрыто", message: "Доступно только в Pro", type: .lock)
+            UserDefaults.standard.set(virtualSection, forKey: "pickedSectionNumber")
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
