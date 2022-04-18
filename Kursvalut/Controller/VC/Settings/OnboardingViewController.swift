e46c0e2be894ba847854537d4112f7e125c77662
artexhibit <vendabal@yandex.ru>
2022-04-18 22:05:39 +0300

    Add a code for previousButtonClicked and nextButtonClicked, implement scrollViewDidEndDecelerating to switch pageControl active indicator, which represents current page, fix some constraints in OnboardingVC

--- Kursvalut/Controller/VC/Settings/OnboardingViewController.swift
+++ Kursvalut/Controller/VC/Settings/OnboardingViewController.swift
@@ -7,9 +7,14 @@ class OnboardingViewController: UIViewController {
     @IBOutlet weak var navigationView: UIVisualEffectView!
     @IBOutlet weak var pageControl: UIPageControl!
     
+    private var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+        }
+    }
     let slides = [
         OnboardingSlide(title: "Kursvalut", subtitle: "Конвертер валют по курсу ЦБ РФ, который Вам понравится", image: UIImage(named: "app.icon")!),
-        OnboardingSlide(title: "Валюты", subtitle: "Следите за изменением курса валют ЦБ РФ по отношению к рублю. Если у вас есть Pro версия, то включив в настройках свою сортировку, вы можете сделать свайп справа налево по любой из ячеек и активировать режим перемещения, нажав на синюю иконку", image: UIImage(named: "0img")!)
+        OnboardingSlide(title: "Валюты", subtitle: "Следите за изменением курса валют ЦБ РФ по отношению к рублю. Если у вас Pro версия: выберите режим сортировки в настройках - Своя. Сделав свайп к левой части экрана, как на картинке, активируется режим перемещения. Расположите валюты в нужном порядке и сохраните результат.", image: UIImage(named: "currencyViewLight")!)
     ]
     
     override func viewDidLoad() {
@@ -19,13 +24,34 @@ class OnboardingViewController: UIViewController {
         pageControl.numberOfPages = slides.count
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+        let indexPath = IndexPath(item: currentPage, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .top, animated: true)
+    }
+    
     @IBAction func closeButtonClicked(_ sender: UIButton) {
+        dismiss(animated: true)
     }
     
     @IBAction func prevButtonClicked(_ sender: UIButton) {
+        if currentPage != 0 {
+            currentPage -= 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
+        if currentPage == slides.count - 1 {
+            //hide onboarding
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
     }
 }
 
@@ -51,12 +77,17 @@ extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDa
             cell.imageView.image = slides[indexPath.row].image
             cell.titleLabel.text = slides[indexPath.row].title
             cell.subtitleLabel.text = slides[indexPath.row].subtitle
-            cell.imageView.layer.cornerRadius = 10
             return cell
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+        pageControl.currentPage = currentPage
     }
 }
