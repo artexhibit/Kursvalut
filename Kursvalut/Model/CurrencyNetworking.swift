fd49611f2aed8b31bdedcc8983e3b89d69c3562b
artexhibit <vendabal@yandex.ru>
2022-07-08 20:58:10 +0300

    Fix an error when new data wasn't saved to core data database

--- Kursvalut/Model/CurrencyNetworking.swift
+++ Kursvalut/Model/CurrencyNetworking.swift
@@ -75,10 +75,10 @@ struct CurrencyNetworking {
                     dataDict.forEach { url, data in
                         self.parseJSON(with: data, from: url)
                     }
+                    coreDataManager.save()
                 }
                 completion(nil)
             }
-            coreDataManager.save()
             pickedDataSource == "ЦБ РФ" ? UserDefaults.standard.setValue(updateTime, forKey: "bankOfRussiaUpdateTime") : UserDefaults.standard.setValue(updateTime, forKey: "forexUpdateTime")
         }
     }
